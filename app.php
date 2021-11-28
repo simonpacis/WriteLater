9f7cd24bb80761047351fa7968b7fc6ca26ddc3d
Simon Pacis <simonpacis@MacBook-Pro.local>
2021-11-27 22:54:12 -0800

    Path creation fix.

--- app.php
+++ app.php
@@ -118,10 +118,11 @@ class App
 	}
 
 	function createPath($path) {
-		if (is_dir($path)) return true;
-		$prev_path = substr($path, 0, strrpos($path, '/', -2) + 1 );
-		$return = $this->createPath($prev_path);
-		return ($return && is_writable($prev_path)) ? mkdir($path) : false;
+		if(!file_exists($path))
+		{
+			mkdir($path, 0777, true);
+		}
+		return true;
 	}
 
 	public function makeFile($path, $pretag)
